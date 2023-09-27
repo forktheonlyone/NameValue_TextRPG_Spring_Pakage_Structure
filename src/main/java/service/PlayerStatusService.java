c19b466e326632b3854c8a991ee5215db46d5eb9
mearae <>
2023-09-27 14:09:33 +0900

    service 생성

---
+++ src/main/java/service/PlayerStatusService.java
@@ -0,0 +1,14 @@
+package service;
+
+import dto.CharactorDto;
+
+public class PlayerStatusService {
+    public void HpChange(int cHp){}
+
+    public void GoldChange(int cGold){}
+
+    public void save(CharactorDto charactorDto){
+
+    }
+
+}
